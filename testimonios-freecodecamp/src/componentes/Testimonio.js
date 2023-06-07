fea23f10fc91696ecb30c2c0a3284129740c7aac
victoriajm07 <victoria10444@gmail.com>
2023-06-08 00:44:54 +0200

    Initial commit

---
+++ testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Testimonio() {
+  return (
+    <div className='contenedor-testimonio'>
+        <img 
+          className='imagen-testimonio'
+          src={required('../imagenes/testimonio2.png')}
+          alt='Foto de Juana'/>
+    </div>
+  );
+
+}
