bc813fc46413d685d4c72ab6ca7826abf1c3721e
victoriajm07 <victoria10444@gmail.com>
2023-06-09 15:05:32 +0200

    Final primer proyecto con React
    Se ha finalizado el primer proyecto que es una página web en React en el minuto 2:50:00 del vídeo https://www.youtube.com/watch?v=6Jfk8ic3KVk

--- testimonios-freecodecamp/src/App.js
+++ testimonios-freecodecamp/src/App.js
@@ -4,7 +4,32 @@ import {Testimonio}  from './componentes/Testimonio';
 function App() {
   return (
     <div className="App">
-      <Testimonio />
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos</h1>
+        <Testimonio 
+        nombre='Emma Bostian'
+        pais='Suecia'
+        imagen='2'
+        cargo='Ingeniera de Software'
+        empresa='Spotify'
+        testimonio='Esto es una prueba de testimonio de nuestros alumnos que para este caso es de Juana'/>
+
+        <Testimonio
+        nombre='Manuel Jiménez'
+        pais='Singapur'
+        imagen=''
+        cargo='Ingeniero de Software'
+        empresa='Amazon'
+        testimonio='Esto es una prueba de testimonio de nuestros alumnos que para este caso es manuel'/>
+
+        <Testimonio
+        nombre='Gonzalo García'
+        pais='Camerún'
+        imagen='3'
+        cargo='Ingeniero de Software'
+        empresa='ChatDesk'
+        testimonio='Esto es una prueba de testimonio de nuestros alumnos que para este caso es Gonzálo'/>
+      </div>
     </div>
   );
 }
--- testimonios-freecodecamp/src/componentes/Testimonio.js
+++ testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import '../hojas-de-estilo/Testimonio.css'
 
-export function Testimonio() {
+export function Testimonio(props) {
   return (
     <div className='contenedor-testimonio'>
         <img 
           className='imagen-testimonio'
-          src={require('../imagenes/testimonio2.png')}
+          src={require(`../imagenes/testimonio${props.imagen}.png`)}
           alt='Foto de Juana'/>
         <div className='contenedor-texto-testimonio'>
-          <p className='nombre-testimonio'>Juana Bostian en Suecia</p>
-          <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
-          <p className='texto-testimonio'>Esto es una prueba del testimonio de Juana</p>
+          <p className='nombre-testimonio'> <strong>{props.nombre}</strong> en {props.pais}</p>
+          <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
+          <p className='texto-testimonio'>"{props.testimonio}"</p>
         </div>
     </div>
   );
