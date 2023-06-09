6972117e294e71c465fd3ba0a94de24012a82cc5
victoriajm07 <victoria10444@gmail.com>
2023-06-09 13:01:17 +0200

    Mostrar el primer testimonio

--- testimonios-freecodecamp/src/componentes/Testimonio.js
+++ testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-function Testimonio() {
+export function Testimonio() {
   return (
     <div className='contenedor-testimonio'>
         <img 
           className='imagen-testimonio'
-          src={required('../imagenes/testimonio2.png')}
+          src={require('../imagenes/testimonio2.png')}
           alt='Foto de Juana'/>
+        <div className='contenedor-texto-testimonio'>
+          <p className='nombre-testimonio'>Juana Bostian en Suecia</p>
+          <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
+          <p className='texto-testimonio'>Esto es una prueba del testimonio de Juana</p>
+        </div>
     </div>
   );
-
 }
+
