baee531cae21badccb996921dfb9fbbf36c5e07f
victoriajm07 <victoria10444@gmail.com>
2023-06-26 13:29:31 +0200

    contador a medias

--- contador-de-clics/src/App.js
+++ contador-de-clics/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
+
+  const manejarClic = () => {
+    console.log("Clic");
+  }
+
+  const reiniciarContador = () => {
+    console.log("reiniciar")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='freecodecamp-logo-contenedor'>
+        <img className='freecodecamp-logo'
+        src={freeCodeCampLogo}
+        alt='Logo de freecodeCamp'/>
+      </div>
+
+      <div className='contenedor-principal'>
+        <Boton 
+         texto='Clic'
+         csBotonDeClic={true}
+         manejarClic={manejarClic}/>
+        <Boton 
+         texto='Reiniciar'
+         csBotonDeClic={false}
+         manejarClic={reiniciarContador}/>
+      </div>
+      
     </div>
   );
 }
